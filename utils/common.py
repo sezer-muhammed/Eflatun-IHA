15078584acc29a4edb2406cba558b801b7a43fc0
sezer-muhammed <e230531@metu.edu.tr>
2022-10-22 17:20:05 +0300

    laoder objects added

--- utils/common.py
+++ utils/common.py
@@ -3,7 +3,10 @@ from pascal_voc_writer import Writer
 from pathlib import Path
 from typing import Tuple
 import os
+import cv2 
 
+from PIL import Image
+import xml.etree.ElementTree as ET
 
 def save_np2pascal(
     data: np.ndarray, out_file: Path, img_path: Path, shape: Tuple[int, int]
@@ -70,16 +73,57 @@ def save_np2yolo(
     yolo_file.close()
 
 
-def load_yolo():
+def load_yolo(in_file: Path, img_path: Path) -> np.ndarray:
+
+    yolo_file = open(in_file, "r")
+    data = yolo_file.read()
+    yolo_file.close()
+
+    data = data.split("\n")
+    data = data[:-1]
+
+    data = [i.split(" ") for i in data]
+    data = np.array(data, dtype = np.float32)
+
+    if len(data.shape) == 1: #shape correction
+        data = np.empty((0, 5))
+
+    im = Image.open(img_path)
+    width, height = im.size
+
+    data[:, 1] = data[:, 1] * width
+    data[:, 2] = data[:, 2] * height
+    data[:, 3] = data[:, 3] * width
+    data[:, 4] = data[:, 4] * height
+
+    data = np.array(data, dtype = np.int32)
+
+    return data
+
+
+def load_coco(in_file: Path, img_path: Path = None) -> np.ndarray:
     raise NotImplementedError("This method is not implemented yet.")
 
 
-def load_coco():
-    raise NotImplementedError("This method is not implemented yet.")
+def load_pascal(in_file: Path, img_path: Path = None) -> np.ndarray:
 
+    tree = ET.parse(in_file, parser = ET.XMLParser(encoding = 'iso-8859-5'))
+    root = tree.getroot()
 
-def load_pascal():
-    raise NotImplementedError("This method is not implemented yet.")
+    list_with_all_boxes = []
+
+    for boxes in root.iter('object'):
+
+        ymin = int(boxes.find("bndbox/ymin").text)
+        xmin = int(boxes.find("bndbox/xmin").text)
+        ymax = int(boxes.find("bndbox/ymax").text)
+        xmax = int(boxes.find("bndbox/xmax").text)
+        name = str(boxes.find("name").text)
+
+        list_with_single_boxes = [xmin, ymin, xmax, ymax, name]
+        list_with_all_boxes.append(list_with_single_boxes)
+
+    return np.array(list_with_all_boxes, dtype = object)
 
 
 if __name__ == "__main__":
@@ -93,11 +137,21 @@ if __name__ == "__main__":
     fake_np_array_id = np.array(
         [[0, 0, 500, 500, 0.54, 0], [10, 40, 900, 120, 0.2, 1]]
     )
+
+    #fake_np_array_id = np.empty((0,5), int)
+
     fake_shape = (3000, 1500)
     fake_path_pascal = Path("temp/label_saver.xml")
     fake_path_yolo = Path("temp/label_saver.txt")
-    fake_img = Path("fakeimg.jpg")
+    fake_img = Path("temp/fakeimg.jpg")
+
+    cv2.imwrite(str(fake_img), np.zeros(fake_shape + (3,)))
 
     save_np2pascal(fake_np_array, fake_path_pascal, fake_img, fake_shape)
     save_np2yolo(fake_np_array_id, fake_path_yolo, fake_img, fake_shape)
     #save_np2coco(fake_np_array, fake_path, fake_img, fake_shape)
+
+    load_pascal(Path("temp\label_saver.xml"))
+    load_yolo(Path("temp/label_saver.txt"), Path("temp/fakeimg.jpg"))
+    #load_coco(Path("temp/label_saver.txt"), Path("temp/fakeimg.jpg"))
+    
