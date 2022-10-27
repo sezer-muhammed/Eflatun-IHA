1ba360edfa85f591c0b263c1b9ddc360034fb111
sezer-muhammed <e230531@metu.edu.tr>
2022-10-27 19:05:08 +0300

    video2frame v2

--- cmd_tools/video2frame.py
+++ cmd_tools/video2frame.py
@@ -7,4 +7,17 @@ parser.add_argument('--infolder', type=str, default=None, help='input folder')
 parser.add_argument('--hz', type=float, default=1, help='frame rate (Hz)')
 
 opt = parser.parse_args()
-print(opt.hz)
+
+import os
+import cv2
+import glob
+from pathlib import Path
+
+def video2frame(infile, hz):
+    infile = Path(infile)
+
+    camera = cv2.VideoCapture(str(infile))
+    fps=camera.get(cv2.CAP_PROP_FPS)
+    period = round(fps/hz)
+
+
