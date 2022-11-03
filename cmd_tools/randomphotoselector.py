4ef3a1987979b8f154de6bb6234a0a4d4f634afa
GaryH0lt <cakmaktepesemih@gmail.com>
2022-11-03 18:19:10 +0300

    randomphotoselector v2

--- cmd_tools/randomphotoselector.py
+++ cmd_tools/randomphotoselector.py
@@ -3,19 +3,23 @@ import random
 from pathlib import Path
 import glob
 import shutil
+import os
 
 parser = argparse.ArgumentParser(description="Selects random photos from given folder")
 parser.add_argument('--infolder', type=str, default=None, help='input folder')
 parser.add_argument('--count', type=float, default=1, help='number of photos you want to select')
 parser.add_argument('--outfolder', type=str, default=None, help='output folder')
+parser.add_argument('--copy', type=str, default=None, help='copy folder')
 opt = parser.parse_args()
 
-def photoselector(infile: Path, count: int, outfolder: Path):
+def photoselector(infile: Path, count: int, outfolder: Path, copyfile: Path):
+    
     files = [f for f in infile.iterdir() if f.is_file()]
     randomfiles = random.sample(files, count)
     for f in randomfiles:
         shutil.copy(f, outfolder)
+        shutil.move(f, copyfile)
 
 if __name__ == '__main__':
-    photoselector(Path(opt.infolder), int(opt.count), Path(opt.outfolder))
+    photoselector(Path(opt.infolder), int(opt.count), Path(opt.outfolder), Path(opt.copy))
     
