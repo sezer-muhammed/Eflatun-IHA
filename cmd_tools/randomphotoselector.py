2470bfe6d3fa3ee404a7303b074e5deb36ca6a56
GaryH0lt <cakmaktepesemih@gmail.com>
2022-11-04 18:11:16 +0300

    randomphotoselector v3

--- cmd_tools/randomphotoselector.py
+++ cmd_tools/randomphotoselector.py
@@ -12,14 +12,18 @@ parser.add_argument('--outfolder', type=str, default=None, help='output folder')
 parser.add_argument('--copy', type=str, default=None, help='copy folder')
 opt = parser.parse_args()
 
-def photoselector(infile: Path, count: int, outfolder: Path, copyfile: Path):
+def photoselector(infile: Path, count: int, outfolder: str, copyfile: Path):
     
     files = [f for f in infile.iterdir() if f.is_file()]
     randomfiles = random.sample(files, count)
+    name = str(outfolder)
+    new_dir = os.mkdir(name+"_random_selected_photos")
+    new_dir = os.path.join(os.getcwd(), name+"_random_selected_photos")
+    
     for f in randomfiles:
-        shutil.copy(f, outfolder)
+        shutil.copy(f, Path(new_dir))
         shutil.move(f, copyfile)
 
 if __name__ == '__main__':
-    photoselector(Path(opt.infolder), int(opt.count), Path(opt.outfolder), Path(opt.copy))
+    photoselector(Path(opt.infolder), int(opt.count), str(opt.outfolder), str(opt.copy))
     
