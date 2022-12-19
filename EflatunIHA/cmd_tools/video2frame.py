4bf03133dc7f56a4b98cf5cfff430195d017dcc3
sezer-muhammed <e230531@metu.edu.tr>
2022-12-19 17:11:46 +0300

    restart

--- EflatunIHA/cmd_tools/video2frame.py
+++ EflatunIHA/cmd_tools/video2frame.py
@@ -15,23 +15,22 @@ from pathlib import Path
 def video2frame(infile: Path, hz: float, outfile: Path):
 
     camera = cv2.VideoCapture(str(infile))
+    outfile.mkdir(parents=True, exist_ok=True)
 
     counter = 0
 
     fps = camera.get(cv2.CAP_PROP_FPS)
     period = round(fps / hz)
-    print(f"Starting operation with {fps} fps and {hz} Hz, {infile}")
+    print(f"Starting operation with {fps} fps and {hz} Hz, {infile}, skipping {period-1} frames")
 
     while True:
         counter += 1
 
-        camera.set(cv2.CAP_PROP_POS_FRAMES, (counter * period) - 1)
+        camera.set(cv2.CAP_PROP_POS_FRAMES, max((counter * period) - 1, 1))
         ret, frame = camera.read()
 
         if ret is False:
             break
-
-        outfile.mkdir(parents=True, exist_ok=True)
 
         frame_name = f"{period}_{counter}_{infile.stem}_teknofest2023.jpg"
 
@@ -44,5 +43,9 @@ def video2frame(infile: Path, hz: float, outfile: Path):
 videos = glob.glob(opt.infolder + "/**.*", recursive=True)
 
 for video in videos:
-    if video.endswith((".mp4", ".mov", ".MP4", ".MOV")):
-        video2frame(Path(video), opt.hz, Path("frames"))
+    try:
+        if video.endswith((".mp4", ".mov", ".MP4", ".MOV")):
+            video2frame(Path(video), opt.hz, Path("frames"))
+    except:
+        print(f"Passing {video}")
+        continue
