86667c2e5d238d5172d1dc05fa08db4d5283c144
sezer-muhammed <e230531@metu.edu.tr>
2022-12-24 11:02:42 +0300

    random added

--- EflatunIHA/cmd_tools/label2view.py
+++ EflatunIHA/cmd_tools/label2view.py
@@ -3,18 +3,19 @@ import argparse
 parser = argparse.ArgumentParser(
     description="Draw Labels On The Image Then Save"
 )
-parser.add_argument('--infolder', type=str, default=None, help='input folder')
+parser.add_argument("-f", '--infolder', type=str, default=None, help='input folder')
 parser.add_argument(
-    '--label', type=str, default=None, help='label type: "pascal" or "yolo"'
+    "-l", '--label', type=str, default=None, help='label type: "pascal" or "yolo"'
 )
 parser.add_argument(
-    '--classes',
+    "-c", '--classes',
     type=str,
     default=None,
     help='label classes config name: "TCSU" or "TCDUN" or "TCU"'
 )
-parser.add_argument('--show', action='store_true', help='show image realtime')
+parser.add_argument("-s", '--show', action='store_true', help='show image realtime')
 parser.add_argument('--save', action='store_true', help='save images')
+parser.add_argument("-r", '--random', action='store_true', help='randomize')
 
 opt = parser.parse_args()
 
@@ -22,6 +23,7 @@ from pathlib import Path
 import sys
 import os
 import cv2
+from random import shuffle
 
 #* taken from yolov5 repo
 FILE = Path(__file__).resolve()
@@ -53,38 +55,48 @@ images = [
     if x.endswith((".jpg", ".png", ".JPG", ".PNG"))
 ]
 
+if opt.random:
+    shuffle(images)
+
 for image in images:
-    img_path = os.path.join(in_fold, image)
-    label_path = os.path.join(
-        in_fold,
-        image.replace(".jpg", label_type_mapping[opt.label]).replace(
-            ".png", label_type_mapping[opt.label]
+    try:
+        img_path = os.path.join(in_fold, image)
+        label_path = os.path.join(
+            in_fold,
+            image.replace(".jpg", label_type_mapping[opt.label]).replace(
+                ".png", label_type_mapping[opt.label]
+            )
         )
-    )
-    label = label_loader(
-        label_path, config, img_path, label_loader_mapping[opt.label]
-    )
-    frame = cv2.imread(img_path)
-    print(f"{label}")
-
-    label_data = label.get_data()
-
-    for l in label_data:
-        color = (
-            hash_255(l[-1] + "B"), hash_255(l[-1] + "G"), hash_255(l[-1] + "R")
+        label = label_loader(
+            label_path, config, img_path, label_loader_mapping[opt.label]
         )
-        x1, y1, x2, y2 = l[0], l[1], l[2], l[3]
-        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-        cv2.putText(
-            frame, l[4], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 2.5, color, 3
-        )
+        frame = cv2.imread(img_path)
+        print(f"{label}")
 
-    if opt.show:
-        cv2.imshow("frame", cv2.resize(frame, (0, 0), fx=0.5, fy=0.5))
-        if cv2.waitKey(500) == ord("q"):
-            exit()
+        label_data = label.get_data()
 
-    if opt.save:
-        Path(os.path.join(in_fold,
-                          "labeled_output")).mkdir(parents=True, exist_ok=True)
-        cv2.imwrite(os.path.join(in_fold, "labeled_output", image), frame)
+        for l in label_data:
+            color = (
+                hash_255(l[-1] + "B"), hash_255(l[-1] + "G"), hash_255(l[-1] + "R")
+            )
+            x1, y1, x2, y2 = l[0], l[1], l[2], l[3]
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(
+                frame, l[4], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 2.5, color, 3
+            )
+
+        if opt.show:
+            cv2.imshow("frame", cv2.resize(frame, (0, 0), fx=0.5, fy=0.5))
+            if cv2.waitKey(500) == ord("q"):
+                exit()
+
+        if opt.save:
+            Path(os.path.join(in_fold,
+                            "labeled_output")).mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(os.path.join(in_fold, "labeled_output", image), frame)
+    except Exception as e:
+        print(f"Error: {e}")
+        if opt.save:
+            Path(os.path.join(in_fold,
+                            "labeled_output")).mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(os.path.join(in_fold, "labeled_output", image), cv2.imread(img_path))
